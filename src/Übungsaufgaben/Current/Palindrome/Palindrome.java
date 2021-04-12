5adc76152b80437e9151552dd3c10d8c8c5a7b7b
Philipp Reinig <philipp.reinig@st.ovgu.de>
2021-04-12 23:48:15 +0200

    1.2 Palindrome

--- src/Übungsaufgaben/Current/Palindrome/Palindrome.java
+++ src/Übungsaufgaben/Current/Palindrome/Palindrome.java
@@ -1,12 +1,45 @@
-public class Palindrome {
-  // Test if text is a palindrome.
-  // Ignore upper/lower case, white space, and punctuation.
-  //
-  public static boolean isPalindrome(String text) {
-    // TODO: implementation
-  }
+package Übungsaufgaben.Current.Palindrome;
 
-  public static void main(String[] args) {
-    // TODO: test
-  }
+import java.util.Locale;
+
+
+public class Palindrome {
+    // Test if text is a palindrome.
+    // Ignore upper/lower case, white space, and punctuation.
+    //
+
+    public static boolean isPalindrome(String text) {
+        text = text.toLowerCase(Locale.ROOT).replace(".", "").replace(",", "").replace(";", "").replace(" ", "")
+                .replace("?", "").replace("-", "").replace("!", "");
+        Character[] chars = text.chars().mapToObj(c -> (char) c).toArray(Character[]::new);
+        int mid = (int) Math.round(chars.length / 2.0);
+        Stack<Character> stackFromFront = new Stack<>();
+        Stack<Character> stackFromBack = new Stack<>();
+        for (int i = 0; i < mid; i++) {
+            stackFromFront.push(chars[i]);
+            stackFromBack.push(chars[chars.length - 1 - i]);
+        }
+
+
+        while (!stackFromFront.is_empty()) {
+            System.out.println("sfF: " + stackFromFront.toString());
+            System.out.println("sfB: " + stackFromBack.toString());
+            if (!stackFromFront.pop().toString().equals(stackFromBack.pop().toString())) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] testCases = {"Reliefpfeiler", "Madam", "Lagerregal", "Marktkram", "Ein Esel lese nie.", "Gurken hol ohne Krug!", "Na, Fakir, Paprika-Fan?", "O, Streit irritiert so!", "Eine Horde bedrohe nie!"};
+        boolean allTestsSuccessful = true;
+        for (String testCase : testCases) {
+            System.out.println("testing: " + testCase + "| result: " + isPalindrome(testCase));
+            if (!isPalindrome(testCase)) allTestsSuccessful = false;
+        }
+        if (allTestsSuccessful) {
+            System.out.println("ALL TESTS PASSED SUCCESSFUL");
+        } else {
+            System.out.println("TESTS FAILED");
+        }
+    }
 }
--- src/Übungsaufgaben/Current/Palindrome/Stack.java
+++ src/Übungsaufgaben/Current/Palindrome/Stack.java
@@ -1,34 +1,40 @@
-// package aud;
+package Übungsaufgaben.Current.Palindrome;// package aud;
 
 // import aud.adt.AbstractStack;
+
 import java.util.NoSuchElementException;
 
-/** Implementation of a stack based on {@link aud.Vector}.
+/**
+ * Implementation of a stack based on {@link Vector}.
  */
 public class Stack<T> extends AbstractStack<T> {
-  Vector<T> data_;
+    Vector<T> data_;
 
-  public Stack() { data_=new Vector<T>(); }
+    public Stack() {
+        data_ = new Vector<>();
+    }
 
-  @Override
-  public boolean is_empty() { return data_.empty();  }
+    @Override
+    public boolean is_empty() {
+        return data_.empty();
+    }
 
-  @Override
-  public T top() throws NoSuchElementException {
-    if (data_.empty())
-      throw new NoSuchElementException();
-    return data_.back();
-  }
+    @Override
+    public T top() throws NoSuchElementException {
+        if (data_.empty())
+            throw new NoSuchElementException();
+        return data_.back();
+    }
 
-  public T pop() throws NoSuchElementException {
-    if (data_.empty())
-      throw new NoSuchElementException();
-    T obj=data_.back();
-    data_.pop_back();
-    return obj;
-  }
+    public T pop() throws NoSuchElementException {
+        if (data_.empty())
+            throw new NoSuchElementException();
+        T obj = data_.back();
+        data_.pop_back();
+        return obj;
+    }
 
-  public void push(T x) {
-    data_.push_back(x);
-  }
+    public void push(T x) {
+        data_.push_back(x);
+    }
 }
