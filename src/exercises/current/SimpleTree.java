dac99bbb14cd22107444eb274d928bfbae5376f2
philippreinig <philipp.reinig@st.ovgu.de>
2021-05-13 01:14:57 +0200

    6.1 Eigener SearchTree final

---
+++ src/exercises/current/SimpleTree.java
@@ -0,0 +1,39 @@
+package exercises.current;
+
+//-----------------------------------------------------------------//
+public class SimpleTree<T extends Comparable<T>> {
+    //---------------------------------------------------------------//
+    public class Node {
+        // TODO: implement node class
+    }
+
+    //---------------------------------------------------------------//
+    private Node root_;
+
+    //---------------------------------------------------------------//
+    public SimpleTree() {
+        // TODO: implementation
+    }
+
+    //---------------------------------------------------------------//
+    public void add(T obj) {
+        // TODO: implementation
+    }
+
+    //---------------------------------------------------------------//
+    public boolean contains(T obj) {
+        return false;
+        // TODO: implementation
+    }
+
+    //---------------------------------------------------------------//
+    public String toString() {
+        return "";
+        // TODO: implementation
+    }
+
+    //---------------------------------------------------------------//
+    public static void main(String[] args) {
+        // TODO: test your code with appropriate examples
+    }
+}
