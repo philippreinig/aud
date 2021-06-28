c2b5e1f2d6d52607641b83a3d31509ce53d6503b
philippreinig <philipp.reinig@gmail.com>
2021-06-28 23:20:55 +0200

    week 10 1 and 2 final

--- src/exercises/week10/AdjMatrix.java
+++ src/exercises/week10/AdjMatrix.java
@@ -41,31 +41,33 @@ public class AdjMatrix{
     public static boolean hasTriangle(int[][] m) {
         int n = m.length;
         for(int i = 0; i < n; ++i){
-            for(int j = 0; j < n; ++j){
-                if (m[i][j] != 0){
-                    for(int k = 0; k < n; k++){
-                        if (m[j][k] != 0){
-                            System.out.println("matrix has triangle for " + j + ", " + k);
+            List<Integer> first = adjacent(i, m);
+            for(Integer j : first){
+                List<Integer> second = adjacent(j, m);
+                for(Integer k : second){
+                    List<Integer> third = adjacent(k, m);
+                    for(Integer l : third){
+                        if (i == l){
+//                            System.out.println(i + ", " + j + ", " + k + ", " + l);
                             return true;
                         }
                     }
-
                 }
+
             }
         }
         return false;
 
     }
 
-    //-------------------------------------------------------------//
     public static void main(String args[]) {
         int[][] adjacencyMatrix =
                 {
-                    {0,1,1,0,0},
-                    {1,0,0,1,0},
+                    {0,1,0,0,1},
+                    {0,0,0,1,0},
+                    {0,1,0,0,0},
                     {0,0,1,0,0},
-                    {1,1,1,0,1},
-                    {1,0,0,0,1}
+                    {0,0,0,1,0}
                 };
         System.out.println(Arrays.deepToString(adjacencyMatrix).replace("], ", "]\n").substring(1, (Arrays.deepToString(adjacencyMatrix).replace("], ", "]\n").length()-1)));
         for(int i = 0; i < adjacencyMatrix.length; ++i){
