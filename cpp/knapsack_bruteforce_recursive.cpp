ed56b5da3a9f198e82e2e433bf4e04b8a19f43a7
philippreinig <philipp.reinig@gmail.com>
2021-07-10 20:45:52 +0200

    10.07.2021 20:45

--- cpp/knapsack_bruteforce_recursive.cpp
+++ cpp/knapsack_bruteforce_recursive.cpp
@@ -30,7 +30,7 @@ PerfectLoot max_profit(int i, int cap, ITEMS& loot){
     ++counter;
     if (i == 0){
         return PerfectLoot{0, ITEMS{}};
-        }
+    }
     else{
         if (loot[i].weight > cap) return max_profit(i-1, cap, loot);
         else{
@@ -46,19 +46,27 @@ PerfectLoot max_profit(int i, int cap, ITEMS& loot){
     }
 }
 
+void print_knapsack(ITEMS& knapsack){
+    for(size_t i = 0; i < knapsack.size(); ++i){
+        std::cout << knapsack[i] << ", ";
+    }
+    std::cout << std::endl;
+}
+
 int main(){
     ITEMS loot = {Item{1,6}, Item{5,4}, Item{4,3}, Item{3,2}, Item{6,4}, Item{2,3}, Item{1,5}, Item{7, 3}};
+    std::cout << "available loot is: " << std::endl;
+    print_knapsack(loot);
 
     int capacity = 10;
 
     PerfectLoot optimal_loot = max_profit(loot.size(), capacity, loot);
 
-    std::cout << optimal_loot.value << std::endl;
+    std::cout << "highest loot value possible: " << optimal_loot.value << std::endl;
 
-    std::cout << "looted following items: " << std::endl;
-    for(size_t i = 0; i < optimal_loot.items.size(); ++i){
-        std::cout << optimal_loot.items[i] << std::endl;
-    }
+    std::cout << "looted following items: ";
+    
+    print_knapsack(optimal_loot.items);
 
     std::cout << "decisions to determine perfect loot: " << counter << std::endl;
 }
--- src/lectures/graphs/tests/DijkstrasAlgorithmTest.java
+++ src/lectures/graphs/tests/DijkstrasAlgorithmTest.java
@@ -3,23 +3,77 @@ package lectures.graphs.tests;
 import aud.util.DotViewer;
 import lectures.graphs.DijkstrasAlgorithm;
 import lectures.graphs.DirectedGraph;
-import lectures.graphs.util.GraphUtilities;
+import lectures.graphs.Edge;
+import lectures.graphs.Vertex;
 import util.Utilities;
-
-import java.util.Arrays;
 
 public class DijkstrasAlgorithmTest {
 
     public static void main(final String[] args) {
-        final DirectedGraph<Integer> graph = GraphUtilities.createRandomGraph(10, 15, 100);
-        System.out.println(Arrays.toString(graph.getEdgesArray()));
+//        final DirectedGraph<Integer> graph = GraphUtilities.createRandomGraph(10, 15, 100);
+//        System.out.println(Arrays.toString(graph.getEdgesArray()));
+//        DotViewer.displayWindow(graph.toDot(), null);
+//
+//        final DijkstrasAlgorithm.DijkstrasAlgorithmResult dar = DijkstrasAlgorithm.runDijkstrasAlgorithm(graph, graph.getVertices().get(0));
+//        System.out.println("shortest paths: ");
+//        Utilities.printMap(dar.getShortestPathsPredecessorVertices());
+//        System.out.println("shortest distances: ");
+//        Utilities.printMap(dar.getShortestPathsDistances());
+
+        final DirectedGraph<String> graph = new DirectedGraph<>();
+        final Vertex<String> berlin = new Vertex<>(graph, "Berlin");
+        final Vertex<String> detroit = new Vertex<>(graph, "Detroit");
+        final Vertex<String> chicago = new Vertex<>(graph, "Chicago");
+        final Vertex<String> lissabon = new Vertex<>(graph, "Lissabon");
+        final Vertex<String> frankfurt = new Vertex<>(graph, "Frankfurt");
+        final Vertex<String> paris = new Vertex<>(graph, "Paris");
+        final Vertex<String> toronto = new Vertex<>(graph, "Toronto");
+        final Vertex<String> newyork = new Vertex<>(graph, "New York");
+
+        graph.addNode(berlin);
+        graph.addNode(detroit);
+        graph.addNode(chicago);
+        graph.addNode(lissabon);
+        graph.addNode(frankfurt);
+        graph.addNode(paris);
+        graph.addNode(toronto);
+        graph.addNode(newyork);
+
+
+        final Edge e1 = new Edge(berlin, frankfurt, 1);
+        final Edge e2 = new Edge(berlin, paris, 2);
+        final Edge e3 = new Edge(berlin, lissabon, 3);
+        final Edge e4 = new Edge(frankfurt, newyork, 6);
+        final Edge e5 = new Edge(frankfurt, toronto, 5);
+        final Edge e6 = new Edge(paris, newyork, 5);
+        final Edge e7 = new Edge(paris, toronto, 6);
+        final Edge e8 = new Edge(lissabon, toronto, 4);
+        final Edge e9 = new Edge(lissabon, chicago, 5);
+        final Edge e10 = new Edge(newyork, detroit, 3);
+        final Edge e11 = new Edge(toronto, detroit, 3);
+        final Edge e12 = new Edge(chicago, detroit, 4);
+
+        graph.addEdge(e1);
+        graph.addEdge(e2);
+        graph.addEdge(e3);
+        graph.addEdge(e4);
+        graph.addEdge(e5);
+        graph.addEdge(e6);
+        graph.addEdge(e7);
+        graph.addEdge(e8);
+        graph.addEdge(e9);
+        graph.addEdge(e10);
+        graph.addEdge(e11);
+        graph.addEdge(e12);
+
         DotViewer.displayWindow(graph.toDot(), null);
 
-        final DijkstrasAlgorithm.DijkstrasAlgorithmResult dar = DijkstrasAlgorithm.runDijkstrasAlgorithm(graph, graph.getVertices().get(0));
+        final DijkstrasAlgorithm.DijkstrasAlgorithmResult result = DijkstrasAlgorithm.runDijkstrasAlgorithm(graph, berlin);
         System.out.println("shortest paths: ");
-        Utilities.printMap(dar.getShortestPathsPredecessorVertices());
+        Utilities.printMap(result.getShortestPathsPredecessorVertices());
         System.out.println("shortest distances: ");
-        Utilities.printMap(dar.getShortestPathsDistances());
+        Utilities.printMap(result.getShortestPathsDistances());
+
 
     }
 
